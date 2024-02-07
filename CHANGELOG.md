6ced3bdeaecc7f19e2851a75a19317b33d2fc4be
Julien Loizelet <julienloizelet@okaeli.com>
2024-02-07 09:14:54 +0900

    chore(*): Prepare release 0.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,16 @@ functions provided by the `src/cscapi` folder.
 
 --- 
 
+## [0.0.2](https://github.com/crowdsecurity/python-capi-sdk/releases/tag/v1.1.0) - 2024-02-07
+[_Compare with previous release_](https://github.com/crowdsecurity/python-capi-sdk/compare/v0.0.1...v0.0.2)
+
+
+### Fixed
+
+- Enable foreign key constraints only in SQLite connections ([#13](https://github.com/crowdsecurity/python-capi-sdk/pull/13))
+
+---
+
 ## [0.0.1](https://github.com/crowdsecurity/python-capi-sdk/releases/tag/v0.0.1) - 2024-02-06
 
 - Initial release
--- examples/basic.py
+++ examples/basic.py
@@ -20,24 +20,6 @@ signals = [
         machine_id=generate_machine_id_from_key("myMachineKeyIdentifier"),
         context=[{"key": "scenario-version", "value": "1.0.0"}],
         message="test message to see where it is written",
-        decisions=[
-            {
-                "origin": "crowdsec",
-                "duration": "1h",
-                "scenario": "crowdsec/ssh-bf",
-                "scope": "ip",
-                "type": "ban",
-                "value": "81.81.81.81",
-            },
-            {
-                "origin": "pysdk",
-                "duration": "2h",
-                "scenario": "crowdsec/ssh-bf",
-                "scope": "ip",
-                "type": "ban",
-                "value": "81.81.81.81",
-            },
-        ],
     )
 ]
 
